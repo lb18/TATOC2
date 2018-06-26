437be2af3334eb5e7b01ef5b5ffa508f715fba3c
lb18 <40322896+lb18@users.noreply.github.com>
2018-06-26 16:40:16 +0530

    Update TestTatoc.java

--- test/TestTatoc.java
+++ test/TestTatoc.java
@@ -5,14 +5,13 @@ import pages.SelectCourse;
 import pages.GridGate;
 import pages.FrameDungeon;
 import pages.DragAround;
-
+import pages.PopupWindow;
+import pages.GenerateToken;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
-
 import static org.testng.Assert.assertEquals;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -26,6 +25,8 @@ public class TestTatoc
 	GridGate select_box;
 	FrameDungeon box_colors;
 	DragAround drag_and_drop;
+	PopupWindow popup;
+	GenerateToken generate_token;
 	
 	@BeforeClass
 	public void init()
@@ -36,6 +37,8 @@ public class TestTatoc
 		select_box = PageFactory.initElements(driver, GridGate.class);
 		box_colors = PageFactory.initElements(driver, FrameDungeon.class);
 		drag_and_drop = PageFactory.initElements(driver, DragAround.class);
+		popup = PageFactory.initElements(driver, PopupWindow.class);
+		generate_token = PageFactory.initElements(driver, GenerateToken.class);
 	}
 	
 	@Test
@@ -93,7 +96,45 @@ public class TestTatoc
 		WebElement get_the_text_of_first_heading = driver.findElement(By.cssSelector("body > div > div.page > h1"));
 		assertEquals(get_the_text_of_first_heading.getText().contains("Pop"), true);
 	}
-
 	
+	@Test(dependsOnMethods = {"dropbox_Contains_Dragbox_Should_Go_To_Next_Page"})
+	public void empty_Text_Field_Should_Display_Error_Page()
+	{
+		boolean res = popup.text_Field_Is_Empty();
+		assertEquals(res, true);
+		WebElement get_the_text_of_first_heading = driver.findElement(By.cssSelector("body > div > div.page > h1"));
+		Assert.assertTrue(get_the_text_of_first_heading.getText().contains("Error"));
+	}
 	
+	@Test(dependsOnMethods = {"dropbox_Contains_Dragbox_Should_Go_To_Next_Page", "empty_Text_Field_Should_Display_Error_Page"})
+	public void text_Field_Contains_Some_Text_Should_Go_To_Next_Paage()
+	{
+		driver.navigate().back();
+		popup.text_Field_Is_Filled();
+		WebElement get_link_text = driver.findElement(By.linkText("Generate Token"));
+		Assert.assertTrue(get_link_text.getText().contains("Generate Token"));
+	}
+	
+	@Test(dependsOnMethods = {"text_Field_Contains_Some_Text_Should_Go_To_Next_Paage"})
+	public void token_Not_Generated_Should_Display_Error_Page()
+	{
+		generate_token.token_Not_Generated();
+		WebElement get_the_text_of_first_heading = driver.findElement(By.cssSelector("body > div > div.page > h1"));
+		Assert.assertTrue(get_the_text_of_first_heading.getText().contains("Error"));
+	}
+	
+	@Test(dependsOnMethods = {"text_Field_Contains_Some_Text_Should_Go_To_Next_Paage", "token_Not_Generated_Should_Display_Error_Page"})
+	public void token_Generated_Should_Go_To_Next_Paage()
+	{
+		driver.navigate().back();
+		generate_token.token_Generated();
+		WebElement get_the_text_of_first_heading = driver.findElement(By.cssSelector("body > div > div.page > h1"));
+		Assert.assertTrue(get_the_text_of_first_heading.getText().contains("End"));
+	}
+	
+	@AfterClass
+	public void closeBrowser()
+	{
+		driver.quit();
+	}
 }
